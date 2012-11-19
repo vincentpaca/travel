3f6a7d4e1460c10ec6b5216439ff24b7df258268
vincent <vincent@clickinglabs.com>
2012-11-19 14:02:35 +0800

    Initial Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = '93e7c66a4027538edcf2583a452da87f4f2909d737f83115875eda352da2f9c69badb2a976b8a6661d9e3eb010d164e47015f3983b818142294eb863dd8ec74e'
